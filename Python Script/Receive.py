48f891743b29b1f4486ea44db741df103f9a43a6
jules-deponte <julesdeponte@gmail.com>
2022-12-02 17:42:31 +0200

    Initial Commit

---
+++ Python Script/Receive.py
@@ -0,0 +1,32 @@
+import socket
+import pandas as pd
+
+UDP_IP = "192.168.1.59"
+UDP_PORT = 20777
+
+sock = socket.socket(
+    socket.AF_INET, # Internet
+    socket.SOCK_DGRAM
+) # UDP
+
+
+sock.bind(('', UDP_PORT))
+df = pd.DataFrame(columns=['data'])
+
+l = []
+i = 0
+
+while True:
+    data, addr = sock.recvfrom(1464) # buffer size is 1024 bytes
+    # print("received message: %s" % data)
+
+
+    i = i + 1
+
+    d = {'data':data}
+
+    df = df.append(d, ignore_index=True)
+
+    if i % 10 == 0:
+        print(df)
+        df.to_csv(r'C:\Users\jules\OneDrive\F1 2021 Telemetry\Python Script\data.csv')
